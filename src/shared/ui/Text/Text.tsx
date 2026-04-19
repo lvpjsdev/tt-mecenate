44eff52e01602e1d0eacc2479225014cbce4a71d
lvpjsdev <lvp@lvpjsdev.com>
2026-04-19 23:56:36 +0300

    feat: add multi line support in Text ui component

--- src/shared/ui/Text/Text.tsx
+++ src/shared/ui/Text/Text.tsx
@@ -2,10 +2,18 @@ import { Text as RNText } from 'react-native';
 import { stylesheet } from './Text.styles';
 import type { TextProps } from './Text.types';
 
-export function Text({ variant, children, color }: TextProps) {
-  const style = stylesheet[variant];
+export function Text({ variant, children, color, numberOfLines, onTextLayout }: TextProps) {
+  const variantStyle = stylesheet[variant];
 
-  return <RNText style={[style, color ? { color } : undefined]}>{children}</RNText>;
+  return (
+    <RNText
+      style={[variantStyle, color ? { color } : undefined]}
+      numberOfLines={numberOfLines}
+      onTextLayout={onTextLayout}
+    >
+      {children}
+    </RNText>
+  );
 }
 
 Text.displayName = 'Text';
--- src/shared/ui/Text/Text.types.ts
+++ src/shared/ui/Text/Text.types.ts
@@ -1,7 +1,11 @@
+import type { NativeSyntheticEvent, TextLayoutEventData } from 'react-native';
+
 export type TextVariant = 'label' | 'symbol' | 'placeholder' | 'body' | 'h1' | 'h2' | 'title';
 
 export interface TextProps {
   variant: TextVariant;
   children: string;
   color?: string;
+  numberOfLines?: number;
+  onTextLayout?: (e: NativeSyntheticEvent<TextLayoutEventData>) => void;
 }
