d68b82c69812860a4e8f370030e57f9aee33db70
lvpjsdev <lvp@lvpjsdev.com>
2026-05-01 01:10:58 +0300

    feat: send comment

--- src/app/[postId]/index.tsx
+++ src/app/[postId]/index.tsx
@@ -1,11 +1,13 @@
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { PostWithComments } from '@/features/post-with-comments/ui/PostWithComments';
+import { SendComment } from '@/features/send-comment/ui/SendComment/SendComment';
 import { tokens } from '@/shared/styles/tokens';
 
 export default function PostScreen() {
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: tokens.palette.neutral[0] }}>
       <PostWithComments />
+      <SendComment />
     </SafeAreaView>
   );
 }
--- src/features/send-comment/index.ts
+++ src/features/send-comment/index.ts
@@ -1,2 +1,2 @@
-export { SendCommentInput } from './ui/SendCommentInput';
-export type { SendCommentInputProps } from './ui/SendCommentInput.types';
+export { SendCommentInput } from './ui/SendCommentInput/SendCommentInput';
+export type { SendCommentInputProps } from './ui/SendCommentInput/SendCommentInput.types';
